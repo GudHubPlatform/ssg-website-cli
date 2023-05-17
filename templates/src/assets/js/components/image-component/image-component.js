2d1dfc41bd90f5c08fa6fb8e37d3f28922c37ca5
Nazarii Semeniuk <qnazar.semenyuk@gmail.com>
2023-05-17 12:50:43 +0300

    Updated image component in template

--- templates/src/assets/js/components/image-component/image-component.js
+++ templates/src/assets/js/components/image-component/image-component.js
@@ -9,12 +9,15 @@ class ImageComponent extends GHComponent {
 
     async onServerRender() {
         this.src = this.getAttribute('src');
+        this.dataSrc = this.getAttribute('data-src');
+        this.dataUrl = this.getAttribute('data-url');
         this.alt = this.getAttribute('alt');
         this.title = this.getAttribute('title');
         this.lazyload = this.hasAttribute('lazyload');
 
-        if(this.src) {
-            await fetch(this.src + '?mode=ssr');
+        if(this.dataSrc && this.dataUrl) {
+            await fetch(this.dataUrl + '?source=' + this.dataSrc + '&mode=ssr');
+            this.src = this.dataUrl;
         }
 
         super.render(html);
