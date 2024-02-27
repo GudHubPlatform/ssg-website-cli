c450e969783e2563d89703f876488f9bc0307cb0
Kostyantyn Kosyuk <kostyantyn.kosyuk@gmail.com>
2024-02-27 17:58:22 +0200

    feature: added new config template

--- templates/config.mjs
+++ templates/config.mjs
@@ -4,6 +4,8 @@ export const build_folder = 'dist';
 
 export const auth_key = 'aTS7SoYTiLQvFeJ82LQMIUKa5O5anck/YjQ4AZ7M6rgAs/XRr1oKu5bES278QxYPfwliyzUEOXLF+r7K/6YmOA==';
 
+export const index_sitemap = true;
+
 export const chapters = {
     pages: {
         app_id: 30916,
@@ -15,62 +17,137 @@ export const chapters = {
     }
 }
 
+export const routes = [
+    
+]
+
 export const components_list = [
     ...GudhubComponents,
     {
-        tag: 'header-component',
-        src: '/src/assets/js/components/header/header.js'
+        tag: 'contact-us-block',
+        src: '/src/assets/js/components/contact-us-block/contact-us-block.js'
     },
     {
         tag: 'footer-component',
-        src: '/src/assets/js/components/footer/footer.js'
+        src: '/src/assets/js/components/footer/footer.js',
     },
     {
-        tag: 'image-component',
-        src: '/src/assets/js/components/image-component/image-component.js'
-    },
-    {
-        tag: 'columns-component',
-        src: '/src/assets/js/components/columns/columns.js'
-    },
-    {
-        tag: 'team-component',
-        src: '/src/assets/js/components/team/team.js'
-    },
-    {
-        tag: 'team-person',
-        src: '/src/assets/js/components/team/team-person/team-person.js'
-    },
-    {
-        tag: 'form-component',
-        src: '/src/assets/js/components/form/form.js'
-    },
-    {
-        tag: 'hero-component',
-        src: '/src/assets/js/components/hero/hero.js'
-    },
-    {
-        tag: 'text-component',
-        src: '/src/assets/js/components/text/text.js'
-    },
-    {
-        tag: 'call-to-action',
-        src: '/src/assets/js/components/call-to-action/call-to-action.js'
-    },
-    {
-        tag: 'edit-mode',
-        src: '/src/assets/js/components/edit-mode/edit-mode.js'
-    },
-    {
-        tag: 'grid-component',
-        src: '/src/assets/js/components/grid/grid.js'
-    },
-    {
-        tag: 'a-component',
-        src: '/src/assets/js/components/a.js'
-    },
-    {
-        tag: 'b-component',
-        src: '/src/assets/js/components/b.js'
+        tag: 'header-component',
+        src: '/src/assets/js/components/header/header.js',
     }
 ]
+
+export const form_config = [
+    {
+        "id": "main",
+        "title": "Leave Application",
+        "subtitle": "",
+        "mailConfig": {
+            "to": "awesome@email.com",
+            "from": "awesome@email.com",
+            "subject": "%subject%"
+        },
+        "inputs": [
+            {
+                "name": "name",
+                "type": "short",
+                "required": "true",
+                "placeholder": "Name *",
+                "width": 6
+            },
+            {
+                "name": "company",
+                "type": "short",
+                "required": "false",
+                "placeholder": "Company",
+                "width": 6
+            },
+            {
+                "name": "email",
+                "type": "email",
+                "required": "true",
+                "placeholder": "Email *",
+                "errorText": "Enter a valid email",
+                "width": 6
+            },
+            {
+                "name": "phone",
+                "type": "phone",
+                "required": "false",
+                "placeholder": "Phone",
+                "errorText": "Enter a valid phone number",
+                "width": 6
+            },
+            {
+                "name": "message",
+                "type": "textarea",
+                "required": "false",
+                "placeholder": "Comment",
+                "width": 12
+            }
+        ]
+    },
+    {
+        "id": "popup",
+        "title": "Leave Application",
+        "subtitle": "",
+        "mailConfig": {
+            "to": "awesome@email.com",
+            "from": "awesome@email.com",
+            "subject": "%subject%"
+        },
+        "inputs": [
+            {
+                "name": "email",
+                "type": "email",
+                "required": "true",
+                "placeholder": "Email *",
+                "errorText": "Enter a valid email",
+                "width": 6
+            },
+            {
+                "name": "phone",
+                "type": "phone",
+                "required": "false",
+                "placeholder": "Phone",
+                "errorText": "Enter a valid phone number",
+                "width": 6
+            },
+            {
+                "name": "message",
+                "type": "textarea",
+                "required": "false",
+                "placeholder": "Comment",
+                "width": 12
+            }
+        ]
+    },
+    {
+        "id": "small bottom-right popup",
+        "title": "Get a Free Consultation",
+        "subtitle": "",
+        "mailConfig": {
+            "to": "awesome@email.com",
+            "from": "awesome@email.com",
+            "subject": "%subject%"
+        },
+        "inputs": [
+            {
+                "name": "email",
+                "type": "email",
+                "required": "true",
+                "placeholder": "Email *",
+                "errorText": "Enter a valid email",
+                "width": 12
+            },
+            {
+                "name": "phone",
+                "type": "phone",
+                "required": "false",
+                "placeholder": "Phone",
+                "errorText": "Enter a valid phone number",
+                "width": 12
+            }
+        ]
+    }
+];
